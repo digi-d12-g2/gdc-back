fc14d773316b9cf63f56dd142d9da43640d36af4
Théo <theodore.turpin@outlook.fr>
2023-02-02 12:19:23 +0100

    files

---
+++ src/main/java/com/gdc/gdc/GdcApplication.java
@@ -0,0 +1,13 @@
+package com.gdc.gdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gdc/gdc/GdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdc.gdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
