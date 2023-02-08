f3fb79e13ae73551eba8349bd8582edb9d2fb85a
Théo <theodore.turpin@outlook.fr>
2023-02-08 14:59:03 +0100

    fix login route

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -9,8 +9,8 @@ import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.entity.User;
 import com.service.UserService;
+import com.dto.ResponseUserDto;
 
 @RestController
 @RequestMapping("user")
@@ -22,7 +22,7 @@ public class UserController {
     @GetMapping
     public ResponseEntity<?> signIn(@RequestParam String email, @RequestParam String password) throws NotFoundException {
         try {
-            User signInResponse = this.userService.signIn(email, password);
+            ResponseUserDto signInResponse = this.userService.signIn(email, password);
             return ResponseEntity.status(200).body(signInResponse);
         } catch (IllegalArgumentException e) {
             return ResponseEntity.status(400).body(e.getMessage());
--- src/main/java/com/repository/UserRepository.java
+++ src/main/java/com/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.entity.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long>{
-    
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    public User findByEmailAndPassword(String email, String password);
 }
