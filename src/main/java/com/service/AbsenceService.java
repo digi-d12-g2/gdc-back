53e26e3304a03fa91e8bad94759777266b63d758
claragohier <clara.gohier@gmail.com>
2023-02-16 22:15:00 +0100

    gestion compteur ok

--- src/main/java/com/service/AbsenceService.java
+++ src/main/java/com/service/AbsenceService.java
@@ -134,25 +134,28 @@ public class AbsenceService {
 	@Transactional
 	public ResponseAbsenceDto updateAbsence(Long id, Absence absence){
 		
-		if (checkUpdateIsValid(absence)){
-			Absence absenceToUpdate = getAbsence(id);
+		Absence absenceToUpdate = getAbsence(id);
 
-			// System.out.println(getCount(absence));
-			// System.out.println(getCount(absenceToUpdate));
+		if (checkUpdateIsValid(absence, absenceToUpdate)){
 
 			if(absence.getType() != Type.RTT_EMPLOYEUR){
 
-				// if(getCount(absence) > getCount(absenceToUpdate)){
-				// 	Integer diff = getCount(absence) - getCount(absenceToUpdate);
-				// 	this.userService.decrementUserVacations(absence.getUser().getId(), diff);
-				// } else if(getCount(absence) < getCount(absenceToUpdate)) {
-				// 	Integer diff = getCount(absenceToUpdate) - getCount(absence);
-				// 	this.userService.incrementUserVacations(absence.getUser().getId(), diff);
-				// }
+				if(absenceToUpdate.getStatus() == Status.INITIALE){
+					if(getCount(absence) > getCount(absenceToUpdate)){
+						Integer diff = getCount(absence) - getCount(absenceToUpdate);
+						System.out.println("pok  " + diff);
 
-				Integer count = getCount(absence);
-				System.out.println(count);
-				this.userService.decrementUserVacations(absenceToUpdate.getUser().getId(), count);
+						this.userService.decrementUserVacations(absenceToUpdate.getUser().getId(), diff);
+					} else if(getCount(absence) < getCount(absenceToUpdate)) {
+						Integer diff = getCount(absenceToUpdate) - getCount(absence);
+						System.out.println("ok  " + diff);
+						this.userService.incrementUserVacations(absenceToUpdate.getUser().getId(), diff);
+					}
+				} else {
+					Integer count = getCount(absence);
+					System.out.println(count);
+					this.userService.decrementUserVacations(absenceToUpdate.getUser().getId(), count);
+				}
 			}
 
 			absenceToUpdate.setDate_start(absence.getDate_start());
@@ -186,7 +189,7 @@ public class AbsenceService {
 
 		if((getAbsence(id).getType() == Type.RTT_EMPLOYEUR) && getAbsence(id).getStatus() == Status.VALIDEE){
 			this.employerRttService.incrementEmployerRTT(1L, this.employerRttService.getEmployerRTT(1L));
-		} else if ((getAbsence(id).getType() != Type.RTT_EMPLOYEUR) && getAbsence(id).getStatus() == Status.VALIDEE) {
+		} else if ((getAbsence(id).getType() != Type.RTT_EMPLOYEUR) && (getAbsence(id).getStatus() == Status.VALIDEE || getAbsence(id).getStatus() == Status.EN_ATTENTE_VALIDATION)) {
 			Integer count = getCount(getAbsence(id));
 			this.userService.incrementUserVacations(getAbsence(id).getUser().getId(), count);
 		}
@@ -321,23 +324,32 @@ public class AbsenceService {
 		return true;
 	}
 
-	private boolean checkUpdateIsValid(Absence absence){
+	private boolean checkUpdateIsValid(Absence absence, Absence absenceToUpdate){
 
-		if(
-			checkDateStartBeforeDateEnd(absence) &&
-			checkCongesSansSoldeNeedReason(absence) &&
-			checkStatusIsInitialOrRejete(absence) &&
-			checkNoAbsenceInDateRange(absence)
-		) {
-			return true;
-		} else {
-			return false;
-		}
+		if (!checkDateStartBeforeDateEnd(absence)) {
+			throw new ResourceNotFoundException("La date de début doit être inférieur à la date de fin.");
+		} else if (!checkStatusIsInitialOrRejete(absenceToUpdate)) {
+			throw new ResourceNotFoundException("Le status doit être initial ou rejete.");
+		} else if (!checkCongesSansSoldeNeedReason(absence)) {
+			throw new ResourceNotFoundException("Les congés sans soldes nécessitent un motif.");
+		} 
+		
+		// else if (!checkNoAbsenceInDateRange(absence)) {
+		// 	throw new ResourceNotFoundException("Un jour de congé existe déjà sur cette période.");
+		// }
+
+		return true;
+
 	}
 
 	private Boolean checkDatesAreNotPublicHoliday(Absence absence) {
+		System.out.println(absence.getDate_start());
+		System.out.println(absence.getDate_end());
+
 		List<PublicHolidays> publicHolidays = this.publicHolidayRepository.getPublicHolidaysFromTwoDates(absence.getDate_start(), absence.getDate_end());
-		
+		for(PublicHolidays pH : publicHolidays){
+			System.out.println(pH.getLabel());
+		}
 		return publicHolidays.isEmpty();
 	}
 
